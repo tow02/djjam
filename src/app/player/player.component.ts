c3d5eb3384adee93d39edffea7f2274b3cd91e91
sompop <sompop.kulapalanont@gmail.com>
2020-05-26 15:34:56 +0700

    add trackKey

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import APlayer from 'aplayer';
+
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent implements OnChanges {
+ 
+  @Input()
+  trackKey:string;
 
 
   ap;
@@ -14,7 +18,23 @@ export class PlayerComponent implements OnInit {
     
   }
 
-  ngOnInit(): void {
+
+  ngOnChanges():void{
+    if(this.trackKey){
+      //ifHaveTrackKey init APlayer
+      this.ap =  new APlayer({
+        container: document.getElementById('aplayer'),
+        audio: [{
+            name: 'If It Ain t Love',
+            artist: 'Chick Webb',
+            url: 'https://p.scdn.co/mp3-preview/3b95d84ed3f9e56b4863d28ebad25eccd36c90fe?cid=bb200fb215c346448b3c34bbccaac25d',
+            cover: 'https://i.scdn.co/image/ab67616d0000b273a316589b638578d23bada5b1'
+        }]
+    })
+    }
+  }
+
+  /*ngOnInit(): void {
     this.ap =  new APlayer({
       container: document.getElementById('aplayer'),
       audio: [{
@@ -24,6 +44,7 @@ export class PlayerComponent implements OnInit {
           cover: 'https://i.scdn.co/image/ab67616d0000b273a316589b638578d23bada5b1'
       }]
   });
-  }
+  }*/
+
 
 }
