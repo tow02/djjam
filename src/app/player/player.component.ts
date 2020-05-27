f11295e905dcb64a54e0b2fbef4eb9a7ee03be0e
sompop <sompop.kulapalanont@gmail.com>
2020-05-27 10:46:44 +0700

    add isplaying status

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -10,38 +10,66 @@ import APlayer from 'aplayer';
 })
 export class PlayerComponent implements OnInit {
  
-  
-
   ap;
+  isPlaying = false;
+  currentPlayingId:string;
   constructor(private trackService:TrackService) { 
-    
-  }
-
-
   
+  }
 
   ngOnInit():void{
     console.log('initTrackPlayer')
+    this.ap =  new APlayer({
+      container: document.getElementById('aplayer'),
+      loop:'none'
+    })
+    
     this.trackService.onChangeTrack.subscribe((e:TrackEvent ) => {
       
       //found a new track
       console.log('play new track?', e);
       let track = e.track
       if(e.action == 'play' || e.action == 'select')
-      this.ap =  new APlayer({
-        container: document.getElementById('aplayer'),
-        audio: [{
+        if(this.isPlaying){
+          this.ap.pause();
+          
+        }
+        this.ap =  new APlayer({
+          container: document.getElementById('aplayer'),
+          audio:[{
             name: track.name,
             artist: track.artists.map(a => a.name).join(', '),
             url: track.preview_url,
             cover: track.album.images[0].url,
+            id:track.id
         }],
-        loop:'none'
-      })
-      if(e.action == "play")
+          loop:'none'
+        })
+      
+        /*this.ap.list.add([{
+              name: track.name,
+              artist: track.artists.map(a => a.name).join(', '),
+              url: track.preview_url,
+              cover: track.album.images[0].url,
+              id:track.id
+          }])*/
+      if(e.action == "play"){
+        if(this.currentPlayingId != e.track.id){
+          this.ap.skipForward();
+        }
         this.ap.play()
-      if(e.action == "pause" )
+        this.isPlaying = true;
+        this.currentPlayingId = e.track.id;
+      }
+        
+      if(e.action == "pause" ){
         this.ap.pause();
+        this.isPlaying = false;
+      } 
+        
+        
+
+      
     })
     
   }
