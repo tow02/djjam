6bea592956e4eb4da7adeabccac9f38b228c6517
sompop <sompop.kulapalanont@gmail.com>
2020-06-09 14:12:40 +0700

    add all field and update mobile css

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -11,9 +11,12 @@ import { filterByCityName } from "filterbycities"
 export class SignupComponent implements OnInit {
 
   profileForm  =  this.formBuilder.group({
+    email:['', Validators.email],
     djName:['', Validators.required ],
     communityName: ['',Validators.minLength(5)],
     cityName: ['',Validators.minLength(5)],
+    password:['', Validators.minLength(8)],
+    repassword:['', Validators.minLength(8)]
   })
   constructor(private userService:UserService, private formBuilder:FormBuilder) {
      
@@ -23,14 +26,15 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
 
     this.profileForm.valueChanges.subscribe(val => {
-      console.log('val', val)
+      
       this.options = filterByCityName(val.cityName)
       
     })    
   }
 
   onSubmit(){
-    console.log(this.profileForm);
+    console.log(this.profileForm.errors);
+    console.log(this.profileForm.value);
   }
 
 }
