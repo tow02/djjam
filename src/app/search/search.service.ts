0320005f56a6afcf4f5873db0e6f47c53a9cc6b8
modernduck <sompop.kulapalanont@gmail.com>
2020-06-19 14:14:20 +0700

    create search function

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -5,6 +5,21 @@ import { ElasticSearch } from "../models/Elastic"
 import { transformHumanQueryToElasticQuery } from "../utilities/track.utilities"
 import { AuthenticationService } from "../services/authentication.service"
 
+export interface FilterOptions{
+  type?:{
+    [key:string]:string
+  },
+  tempo?:{
+    from?:number,
+    to?:number
+  },
+  duration:{
+    from?:number,
+    to?:number
+  }
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -29,6 +44,35 @@ export class SearchService {
   async query(humanQuery:string, from:number =0, size:number = 20){
 
     return this.queryTrack(transformHumanQueryToElasticQuery(humanQuery, (await this.authen.auth.currentUser).uid ), from, size);
+  }
+
+  async search(query:string,  from:number =0, size:number = 20, options?:FilterOptions){
+    let q = query
+    if(options){
+      if(options.tempo){
+        const from = options.tempo.from?options.tempo.from:0;
+        const to = options.tempo.to?options.tempo.to:999;
+        q += ` bpm:[${from} TO ${to}]`;
+      }
+      if(options.duration){
+        const from = options.duration.from?options.duration.from:0;
+        const to = options.duration.to?options.duration.to:600000
+        q += ` duration:[${from} TO ${to}]`;
+      }
+      if(options.type){
+        const uid =  (await this.authen.auth.currentUser).uid ;
+        Object.keys(options.type).forEach((typeName, index) => {
+          if(index > 0)
+            q += " AND"
+          if(uid)
+            q += `(tags:${typeName} OR (personal_tags:${typeName}^100 AND personal_tags:${uid} ))`
+          else
+          q += `(tags:${typeName})`
+        })
+      }
+    }
+    return this.queryTrack(q, from, size);
+    
   }
   
 
